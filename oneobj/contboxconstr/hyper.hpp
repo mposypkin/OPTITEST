4888a7baaacf43d3636236c300eb4974fd04c6a9
Mikhail Posypkin <mposypkin@gmail.com>
2016-04-13 11:01:07 +0400

    Added new test function

--- oneobj/contboxconstr/hyper.hpp
+++ oneobj/contboxconstr/hyper.hpp
@@ -5,8 +5,8 @@
  * Created on February 22, 2016, 5:12 PM
  */
 
-#ifndef ALUFPENT_HPP
-#define ALUFPENT_HPP
+#ifndef HYPER_HPP
+#define HYPER_HPP
 
 #include <mpproblem.hpp>
 #include <box/box.hpp>
@@ -17,28 +17,28 @@ namespace OPTITEST {
     /**
      * Aluffi Pentini test function
      */
-    class AluffiPentiniObjective : public COMPI::Functor <double> {
+    class HyperObjective : public COMPI::Functor <double> {
     public:
 
-        AluffiPentiniObjective() : mN(2) {
+        HyperObjective() : mN(2) {
         }
 
         double func(const double* x) {
             double v;
-            v = 0.25 * x[0] * x[0] * x[0] * x[0] - 0.5 * x[0] * x[0] + 0.1 * x[0] + 0.5 * x[1] * x[1];
+            v = x[0] * x[0] - x[1] * x[1];
             return v;
         }
 
         void grad(const double* x, double* g) {
-            g[0] = x[0] * x[0] * x[0] - x[0] + 0.1;
-            g[1] = x[1];
+            g[0] = 2 * x[0];
+            g[1] = 2 * x[1];
         }
 
         void hess(const double* x, double* H) {
-            H[0] = 3 * x[0] * x[0] - 1;
+            H[0] = 2
             H[1] = 0;
             H[2] = 0;
-            H[3] = 1;
+            H[3] = 2;
         }
     private:
 
@@ -48,5 +48,5 @@ namespace OPTITEST {
 
 }
 
-#endif /* ALUFPENT_HPP */
+#endif /* HYPER_HPP */
 
